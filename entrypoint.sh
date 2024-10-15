0bd66b2e2b5f84bc6b55bc137b8abbdf43620c59
Damian Duży <dame@zakonfeniksa.org>
2024-10-15 10:37:04 +0200

    Update entrypoint.sh to make sure certain paths exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# make sure /var/cache/naemon/checkresults exists, otherwise naemon won't start
+mkdir -p /var/cache/naemon/checkresults
+
+# make sure /var/log/naemon/archives exists, otherwise naemon-livestatus will complain
+mkdir -p /var/log/naemon/archives
+
 /usr/bin/naemon -v /etc/naemon/naemon.cfg && /usr/bin/naemon /etc/naemon/naemon.cfg
