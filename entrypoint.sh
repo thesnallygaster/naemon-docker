56d0ccc06bcd1b8b6a53c204036afb20fad0a3d7
Damian Duży <dame@zakonfeniksa.org>
2024-10-16 22:31:20 +0200

    exec the command used to run naemon so it receives signals properly (the more you know)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ mkdir -p /var/cache/naemon/checkresults
 # make sure /var/log/naemon/archives exists, otherwise naemon-livestatus will complain
 mkdir -p /var/log/naemon/archives
 
-/usr/bin/naemon -v /etc/naemon/naemon.cfg && /usr/bin/naemon /etc/naemon/naemon.cfg
+/usr/bin/naemon -v /etc/naemon/naemon.cfg
+exec /usr/bin/naemon /etc/naemon/naemon.cfg
